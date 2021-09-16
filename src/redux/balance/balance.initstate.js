2d25ee14de86db8910acbb2cc48bdc835c6017b8
Svitlana Burba <Svitlana.Burba@gmail.com>
2021-09-16 17:30:26 +0200

    updated Balance component

--- src/redux/balance/balance.initstate.js
+++ src/redux/balance/balance.initstate.js
@@ -1,27 +1,5 @@
 const initState = {
-    balance: 112.34,
-    summaryCosts: [
-      {
-        month: 'перша секунда поточного місяця',
-        sum: 'сумма за місяць',
-      },
-      {
-        month: 'перша секунда попедернього місяця',
-        sum: 'сумма за місяць',
-      },
-    ],
-    summaryProfit: [
-      {
-        month: 'перша секунда поточного місяця',
-        sum: 'сумма за місяць',
-      },
-      {
-        month: 'перша секунда поточного місяця',
-        sum: 'сумма за місяць',
-      },
-    ],
-    costs: [],
-    profits: [ ],
+    balance: ' '
 };
 
 export default initState;
--- src/redux/balance/balance.operations.js
+++ src/redux/balance/balance.operations.js
@@ -1,117 +1,23 @@
 import axios from "axios";
 import balanceActions from "./balance.actions";
+import {store} from "../store";
+
+const url = "http://localhost:3001/api/v1";
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNDMzMzUwZDNiNWFlNDJkNDFiMTU5YyIsImlhdCI6MTYzMTgwMjkwM30.RvxVmRp4BNM-mK-svSOrQii667zLI_51iGLlQNdLozs';
 
 
-const url = "http://localhost:3001/api";
 
 axios.default.baseURL = url;
 
-const addCostOperation = (data, jwt) => (dispatch) => {
-  dispatch(balanceActions.addCostRequest());
-  axios
-    .post(`${url}/costs/add`, data, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then(({ data }) => dispatch(balanceActions.addCostSuccess(data)))
-    .catch((error) => dispatch(balanceActions.addCostError(error)));
-};
+const setBalanceOperation = (balance) => (dispatch) => {
+  //const token=store.getState().auth.token;
 
-const deleteCostOperation = (cost, jwt) =>
-  function modal(dispatch) {
-    dispatch(balanceActions.deleteCostRequest());
-    axios
-      .delete(`${url}/costs/${cost.id}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${jwt}`,
-        },
-      })
-      .then(({ data }) =>
-        dispatch(
-          balanceActions.deleteCostSuccess({ id: cost.id, balance: data.balance })
-        )
-      )
-      .catch((error) => {
-        dispatch(balanceActions.deleteCostError(error));
-      });
-  };
-
-const getDateCostCalendar = (data, jwt) => (dispatch) => {
-  dispatch(balanceActions.getCostByDateRequest());
-  console.log(data);
-  axios
-    .get(`${url}/costs/date/${data}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then((data) => {
-      dispatch(balanceActions.getCostByDateSuccess(data));
-    })
-    .catch((error) => dispatch(balanceActions.getCostByDateError(error)));
-};
-const getDateProfitCalendar = (data, jwt) => (dispatch) => {
-  dispatch(balanceActions.getProfitByDateRequest());
-  axios
-    .get(`${url}/profit/date/${data}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then((data) => {
-      dispatch(balanceActions.getProfitByDateSuccess(data));
-    })
-    .catch((error) => dispatch(balanceActions.getProfitByDateError(error)));
-};
-
-const addProfitOperation = (data, jwt) => (dispatch) => {
-  dispatch(balanceActions.addProfitRequest());
-  axios
-    .post(`${url}/profit/add`, data, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then(({ data }) => {
-      dispatch(balanceActions.addProfitSuccess(data));
-    })
-    .catch((error) => {
-      dispatch(balanceActions.addProfitError(error));
-    });
-};
-
-const deleteProfitOperation = (profit, jwt) => (dispatch) => {
-  dispatch(balanceActions.deleteProfitRequest());
-  axios
-    .delete(`${url}/profit/${profit.id}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then(({ data }) =>
-      dispatch(
-        balanceActions.deleteProfitSuccess({ id: profit.id, balance: data.balance })
-      )
-    )
-    .catch((error) => {
-      dispatch(balanceActions.deleteProfitError(error));
-    });
-};
-
-const setBalanceOperation = (balance, jwt) => (dispatch) => {
   dispatch(balanceActions.addBalanceRequest());
   axios
     .patch(`${url}/user`, {balance:balance}, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
+        Authorization: `Bearer ${token}`,
       },
     })
     .then(({ data }) => {
@@ -122,17 +28,18 @@ const setBalanceOperation = (balance, jwt) => (dispatch) => {
     });
 };
 
-const getBalanceOperation = (jwt) => (dispatch) => {
+const getBalanceOperation = () => (dispatch) => {
+  //const token=store.getState().auth.token;
+  
   dispatch(balanceActions.getBalanceRequest());
   axios
     .get(`${url}/user`, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
+        Authorization: `Bearer ${token}`,
       },
     })
     .then(({ data }) => {
-      console.log(data);
       dispatch(balanceActions.getBalanceSuccess(data.balance));
     })
     .catch((error) => {
@@ -141,12 +48,6 @@ const getBalanceOperation = (jwt) => (dispatch) => {
 };
 
 const balanceOperations = {
-  addCostOperation,
-  deleteCostOperation,
-  getDateCostCalendar,
-  addProfitOperation,
-  deleteProfitOperation,
-  getDateProfitCalendar,
   setBalanceOperation,
   getBalanceOperation,
 };
