a28d417bdf6e82ccaec3b6d55343279881914d2c
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-09-22 10:20:17 +0300

    reset scss added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./client/App";
 import { BrowserRouter } from "react-router-dom";
-
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+
+import "modern-normalize/modern-normalize.css";
 import "./stylesheet/main.scss";
 
 ReactDOM.render(
