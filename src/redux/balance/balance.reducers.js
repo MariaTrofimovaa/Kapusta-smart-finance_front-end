cc26363a94f025808a55051c9495b735e3fab730
Svitlana Burba <Svitlana.Burba@gmail.com>
2021-09-23 23:58:01 +0200

    multiple fixes for balance and transactions, removed getBalance, fixed balance update on transaction add and delete

--- src/redux/balance/balance.reducers.js
+++ src/redux/balance/balance.reducers.js
@@ -2,21 +2,26 @@ import { createReducer } from "@reduxjs/toolkit";
 import initState from "./balance.initstate";
 import actions from "./balance.actions";
 import transactionsActions from "../transactions/transactions.actions";
-
+import {getCurrentUserSuccess} from "../auth/auth.actions"
 
 const balanceReducer = createReducer(initState, {  
-  [actions.addBalanceSuccess]: (state, { payload }) => {
+  [actions.setBalanceSuccess]: (state, { payload }) => {
     return { ...state, balance: payload };
   },
   [actions.getBalanceSuccess]: (state, { payload }) => {
     return { ...state, balance: payload };
   },
+  // баланс в сторе появляется после успешной авторизации и получения данных пользователя
+  [getCurrentUserSuccess]: (state, { payload }) => {
+    return { ...state, balance: payload.balance};
+  },
   // баланс в сторе обновляется так же при добавлении/удалении транзакций
    [transactionsActions.addTransactionSuccess]: (state, { payload }) => {
-    return { ...state, balance: payload.updatedBalance.balance};
+    return { ...state, balance: payload.updatedBalance};
   },
   [transactionsActions.deleteTransactionSuccess]: (state, { payload }) => {
-    return { ...state, balance: payload.updatedBalance.balance };
+    console.log('balance reducer, delete transaction. Payload:', payload)
+    return { ...state, balance: payload.updatedBalance};
   }, 
 });
 
--- src/redux/transactions/transactions.operations.js
+++ src/redux/transactions/transactions.operations.js
@@ -118,8 +118,9 @@ const deleteTransaction = (id) => (dispatch) => {
   dispatch(transactionsActions.deleteTransactionRequest());
 
   deleteTransactionApi(id)
-    .then(() => {
-      dispatch(transactionsActions.deleteTransactionSuccess(id));
+    .then(({data}) => {
+     console.log('delete transaction operation, payload:', data)
+      dispatch(transactionsActions.deleteTransactionSuccess(data));
     })
     .catch((error) =>
       dispatch(transactionsActions.deleteTransactionError(error.message))
