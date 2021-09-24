822ce84599f2c389c00838f9f4c0eccfd5326f0c
8Grom8 <80001894+8Grom8@users.noreply.github.com>
2021-09-24 14:15:50 +0300

    added selector

--- src/client/userMount/UserMount.js
+++ src/client/userMount/UserMount.js
@@ -1,22 +1,33 @@
-// import { useSelector, useDispatch } from "react-redux";
-// import { getAuthUserName } from "../../redux/auth/auth-selectors";
+import { useSelector } from "react-redux";
+import { allIncomeOfMonth, allexpenseOfMonth } from "../../redux/report/report.selectors";
 import css from "./UserMount.module.css";
 import Line from "../../assets/images/line.png";
 
 const UserMount = () =>  {
-//   const dispatch = useDispatch();
-//   const name = useSelector(getAuthUserName);
+const expense = useSelector(allexpenseOfMonth);
+const income = useSelector(allIncomeOfMonth);
+
+const userExpense = expense.reduce((acc, { categorySum }) => {
+    acc += categorySum
+    return acc;
+}, 0);
+
+const userIncome = income.reduce((acc, { categorySum }) => {
+    acc += categorySum
+    return acc;
+}, 0);
+
 return (
     <div className={css.userMount}>
         <p  className={css.expense} >
             Расходы :
         </p>
-        {/* <span className={css.userExpense}>{userExpense}</span> */}
+        <span className={css.userExpense}>{userExpense}</span>
     <img className={css.Line} src={Line} alt="line" width="2" height="32" />
         <p  className={css.income} >
             Доходы :
         </p>
-        {/* <span className={css.userIncome}>{userIncome}</span> */}
+        <span className={css.userIncome}>{userIncome}</span>
     </div>
 );
 }
