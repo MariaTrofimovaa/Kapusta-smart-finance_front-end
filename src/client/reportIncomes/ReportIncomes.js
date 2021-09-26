7613dab2588e2b9ec1689bff5aed549c295e35b9
Alona-Mykhaylenko <79852771+Alona-Mykhaylenko@users.noreply.github.com>
2021-09-26 12:20:44 +0200

    fixed go back to main functionality

--- src/client/reportIncomes/ReportIncomes.js
+++ src/client/reportIncomes/ReportIncomes.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllIncomeOfMonth, setActiveCategori } from "../../redux/report/report.operations";
+import {
+  getAllIncomeOfMonth,
+  setActiveCategori,
+} from "../../redux/report/report.operations";
 
 import { allIncomeOfMonth } from "../../redux/report/report.selectors";
-import CategoriesList from "../categoriesList/CategoriesList";
+import CategoriesList from "../../client/CategoriesList/CategoriesList";
 import setActive from "../../shared/setActive/setActive";
-
 
 const ReportIncomes = () => {
   const dispatch = useDispatch();
